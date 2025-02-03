c54f655fe44e7858a169fe4e3279b432748fdd18
Talib Hussain Khan <talibhk161@gmail.com>
2025-02-03 20:34:27 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+// Exercise 1
+
+// Q1
+alert("Welcome to the website");
+// Q2
+
+alert("Error! Please Enter a Valid password");
+
+// Q3
+alert("Welcome to JS Land... \nHappy Coding");
+
+// Q4
+alert("Welcome to JS Land...")
+alert("Happy Coding")
+
+// Q5
+let message = "Hello... I can JS through my web browser's console ";
+
+console.log(message)
+alert(message)
+
+// Exercise 2
+// Q1
+let userName;
+
+// Q2
+let myName = "Talib";
+// Q3
+let message1 = "Hello World";
+alert(message);
+
+// Q4
+let myName1 = "Jhone Doe";
+let age1 = "15 years Old";
+let certiflied = "Certiflied Mobile Application Development";
+alert(myName1);
+alert(age1);
+alert(certiflied);
+
+// Q5
+let a = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(a);
+
+// Q6
+let email = "example@gmail.com"
+alert(`My email address is ${email}`) 
+
+// Q7
+let book = 'A smarter way to learn JavaScript'
+alert(`${book} ${book}`)
+
+// Q8
+let h1 = document.querySelector(".h1");
+h1.innerHTML = "Yah!I can Write HTML content through JavaScript"
+
+// Q9
+let str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(str)
+
+// Exercise 3
+// Q1
+let age = "I am 15 years old";
+alert(age);
+
+// Q5
+let birthYear = 1990
+document.querySelector(".h2").innerHTML = `My Birth Year is ${birthYear}`;
+document.querySelector(".p").innerHTML = `data type of declared variables ${typeof birthYear}`;
